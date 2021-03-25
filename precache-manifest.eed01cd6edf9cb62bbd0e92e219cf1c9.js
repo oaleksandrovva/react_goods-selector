0044598def9fa5ac80b3fcaea6f58db9a1438fb3
Olga Briukhanova <oaleksandrovva@gmail.com>
2021-03-25 17:14:03 +0200

    Updates

---
+++ precache-manifest.eed01cd6edf9cb62bbd0e92e219cf1c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b4b6980f4c58814fe35491c47180f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "56ad0ad19cbe8695b28c",
+    "url": "./static/css/main.8b88c446.chunk.css"
+  },
+  {
+    "revision": "423d1f8f07466d4c3b37",
+    "url": "./static/js/2.0704ab71.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0704ab71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ad0ad19cbe8695b28c",
+    "url": "./static/js/main.840da28e.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
